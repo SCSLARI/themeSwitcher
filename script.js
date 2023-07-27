58c8b1a353f057c9b946b1f0e60d2c085b2f8322
Larissa Spinelli <larissaspinelli03@gmail.com>
2023-07-27 11:30:43 -0300

    theSwitcher

---
+++ script.js
@@ -0,0 +1,17 @@
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
+
+// chk.addEventListener('click', () => {
+//     document.body.classList.toggle('dark')
+// })
+
+// chk.onchange = () => {
+//     document.body.classList.toggle('dark');
+// }
+
+// chk.onclick = () => {
+//     document.body.classList.toggle('dark');
+// }
